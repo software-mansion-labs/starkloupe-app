88bc7c2c90254027cf6c22c61622ea3e360e499c
Roman <barabanov1233@gmail.com>
2023-10-30 21:29:16 +0400

    Add contact button to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,6 @@
-import { JSX, SVGProps } from 'react'
+import { JSX, SVGProps } from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const navigation = [
 	{
@@ -8,7 +10,7 @@ const navigation = [
 			<svg fill="currentColor" viewBox="0 0 24 24" {...props}>
 				<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
 			</svg>
-		),
+		)
 	},
 	{
 		name: 'GitHub',
@@ -21,15 +23,21 @@ const navigation = [
 					clipRule="evenodd"
 				/>
 			</svg>
-		),
-	},
-]
+		)
+	}
+];
 
 export function Footer() {
 	return (
 		<footer className="bg-white">
 			<div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-				<div className="flex justify-center space-x-6 md:order-2">
+				<div className="flex justify-center space-x-6 md:order-2 items-center">
+					<Link
+						href="mailto:hi@walnut.dev"
+						className="inline-block rounded-lg px-2 py-1 text-sm text-gray-400 hover:text-gray-600 hover:bg-gray-100"
+					>
+						Contact
+					</Link>
 					{navigation.map((item) => (
 						<a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
 							<span className="sr-only">{item.name}</span>
@@ -38,9 +46,11 @@ export function Footer() {
 					))}
 				</div>
 				<div className="mt-8 md:order-1 md:mt-0">
-					<p className="text-center text-xs leading-5 text-gray-500">Copyright &copy; {new Date().getFullYear()} Wido Inc. All rights reserved.</p>
+					<p className="text-center text-xs leading-5 text-gray-500">
+						Copyright &copy; {new Date().getFullYear()} Wido Inc. All rights reserved.
+					</p>
 				</div>
 			</div>
 		</footer>
-	)
+	);
 }
