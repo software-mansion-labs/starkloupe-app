beef4ae3826becd99b2ea398b18b7ed5e1ca4e65
Roman Mazur <roman.mazur@hotmail.com>
2023-10-31 11:39:48 +0100

    Walnut title & favicon (#2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Debugger',
-  description: 'Debugger',
-}
+	title: 'Walnut',
+	description: 'Transaction debugger for Starknet'
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} min-h-screen flex flex-col`}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} min-h-screen flex flex-col`}>{children}</body>
+		</html>
+	);
 }
