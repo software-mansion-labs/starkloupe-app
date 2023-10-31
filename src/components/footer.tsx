fea01d2e9434a886c6d9552b7584dca9e349c28c
Roman Mazur <roman.mazur@hotmail.com>
2023-10-31 10:45:07 +0100

    Changed text "Contact" to email icon (#1)
    * Changed text "Contact" to email icon
    
    * Fix twitter handle
    
    * Open footer links in a new tab

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,17 @@ import Link from 'next/link';
 
 const navigation = [
 	{
+		name: 'Email',
+		href: 'mailto:hi@walnut.dev',
+		icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+			<svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+				<path d="M20 4H4C2.9 4 2 4.9 2 6v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM4 6l8 5 8-5v12H4V6z" />
+			</svg>
+		)
+	},
+	{
 		name: 'Twitter',
-		href: 'https://twitter.com/widolabs#',
+		href: 'https://twitter.com/walnut_dev',
 		icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
 			<svg fill="currentColor" viewBox="0 0 24 24" {...props}>
 				<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -32,14 +41,13 @@ export function Footer() {
 		<footer className="bg-white">
 			<div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
 				<div className="flex justify-center space-x-6 md:order-2 items-center">
-					<Link
-						href="mailto:hi@walnut.dev"
-						className="inline-block rounded-lg px-2 py-1 text-sm text-gray-400 hover:text-gray-600 hover:bg-gray-100"
-					>
-						Contact
-					</Link>
 					{navigation.map((item) => (
-						<a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+						<a
+							key={item.name}
+							href={item.href}
+							target="_blank"
+							className="text-gray-400 hover:text-gray-500"
+						>
 							<span className="sr-only">{item.name}</span>
 							<item.icon className="h-6 w-6" aria-hidden="true" />
 						</a>
