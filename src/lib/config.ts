40ca8ce3e532ac0850f9cfe7b8a665bbd49ea8e2
Roman <barabanov1233@gmail.com>
2023-11-11 16:58:10 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const API_URL =
 		: 'https://xyz.joinwido.com';
 
 export const SIMULATIONS_API_URL =
-	process.env.NEXT_PUBLIC_LOCAL_API === 'true' ? 'http://127.0.0.1:3000' : '';
+	process.env.NEXT_PUBLIC_LOCAL_API === 'true' ? 'http://127.0.0.1:3000' : 'https://api.walnut.dev';
