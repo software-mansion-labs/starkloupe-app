fb2288ae6b7cb8bece9fb7dbbb9087658d98b5bd
Roman <barabanov1233@gmail.com>
2023-11-27 21:40:01 +0400

    Event details, trace ux improvements

--- src/components/transaction-page/trace.tsx
+++ src/components/transaction-page/trace.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Call, CallIoDecoded } from '@/lib/transaction';
+import { Call, CallEventDecoded, CallIoDecoded } from '@/lib/transaction';
 import { copyToClipboard, shortenHash } from '@/lib/utils';
 import { ArrowLongRightIcon, ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/components/ui/button';
@@ -105,88 +105,49 @@ function CallChip({ className, ...props }: React.ComponentPropsWithoutRef<'span'
 	);
 }
 
-function CallDetailsIo({
-	inputs,
-	outputs
-}: {
-	inputs?: CallIoDecoded[];
-	outputs?: CallIoDecoded[];
-}) {
-	if ((!inputs || inputs.length <= 0) && (!outputs || outputs.length <= 0)) return '';
+function CallDetailsIo(tables: { name: string; io: CallIoDecoded[] }[]) {
 	return (
-		<div className="border border-neutral-300 rounded-sm my-2 overflow-hidden">
-			<Table>
-				<TableBody>
-					{inputs && inputs.length > 0 && (
-						<>
-							<TableRow className="bg-neutral-100">
-								<TableHead>Input name</TableHead>
-								<TableHead>Type</TableHead>
-								<TableHead>Value</TableHead>
-							</TableRow>
-							{inputs.map((i, index) => (
-								<TableRow key={index}>
-									<TableCell>{i.name}</TableCell>
-									<TableCell>{i.type}</TableCell>
-									<TableCell>
-										{typeof i.value === 'string' ? (
-											<span>{i.value}</span>
-										) : i.type && i.type.slice(-1) === '*' ? (
-											<span>[{CallInputs(i.value)}]</span>
-										) : i.value_formats && i.value_formats.DECIMAL ? (
-											<span>{i.value_formats.DECIMAL}</span>
-										) : (
-											<span>
-												{'{ '}
-												{CallInputs(i.value)}
-												{' }'}
-											</span>
-										)}
-									</TableCell>
-								</TableRow>
-							))}
-						</>
-					)}
-					{outputs && outputs.length > 0 && inputs && inputs.length > 0 && (
-						<TableRow className="border-0">
-							<TableCell></TableCell>
-							<TableCell></TableCell>
-							<TableCell></TableCell>
-						</TableRow>
-					)}
-					{outputs && outputs.length > 0 && (
-						<>
-							<TableRow className="bg-neutral-100">
-								<TableHead>Output name</TableHead>
-								<TableHead>Type</TableHead>
-								<TableHead>Value</TableHead>
-							</TableRow>{' '}
-							{outputs.map((i, index) => (
-								<TableRow key={index}>
-									<TableCell>{i.name}</TableCell>
-									<TableCell>{i.type}</TableCell>
-									<TableCell>
-										{typeof i.value === 'string' ? (
-											<span>{i.value}</span>
-										) : i.type && i.type.slice(-1) === '*' ? (
-											<span>[{CallInputs(i.value)}]</span>
-										) : i.value_formats && i.value_formats.DECIMAL ? (
-											<span>{i.value_formats.DECIMAL}</span>
-										) : (
-											<span>
-												{'{ '}
-												{CallInputs(i.value)}
-												{' }'}
-											</span>
-										)}
-									</TableCell>
-								</TableRow>
-							))}
-						</>
-					)}
-				</TableBody>
-			</Table>
-		</div>
+		tables.some((t) => t.io.length > 0) && (
+			<div className="border border-neutral-300 rounded-sm my-2 overflow-hidden">
+				<Table>
+					<TableBody>
+						{tables.map(
+							(t) =>
+								t.io.length > 0 && (
+									<>
+										<TableRow className="bg-neutral-100">
+											<TableHead>{t.name}</TableHead>
+											<TableHead>Type</TableHead>
+											<TableHead>Value</TableHead>
+										</TableRow>
+										{t.io.map((i, index) => (
+											<TableRow key={index}>
+												<TableCell>{i.name}</TableCell>
+												<TableCell>{i.type}</TableCell>
+												<TableCell>
+													{typeof i.value === 'string' ? (
+														<span>{i.value}</span>
+													) : i.type && i.type.slice(-1) === '*' ? (
+														<span>[{CallInputs(i.value)}]</span>
+													) : i.value_formats && i.value_formats.DECIMAL ? (
+														<span>{i.value_formats.DECIMAL}</span>
+													) : (
+														<span>
+															{'{ '}
+															{CallInputs(i.value)}
+															{' }'}
+														</span>
+													)}
+												</TableCell>
+											</TableRow>
+										))}
+									</>
+								)
+						)}
+					</TableBody>
+				</Table>
+			</div>
+		)
 	);
 }
 
@@ -274,10 +235,10 @@ function CallElements(
 				<div className="flex flex-col bg-neutral-50 rounded-b-sm border-b border-x border-neutral-200 py-1 px-2 text-sm">
 					<div className="max-w-[90vw]">{Details(callDetailsInfo)}</div>
 					<div className="w-fit min-w-[30rem]">
-						{CallDetailsIo({ inputs: call.inputs_decoded, outputs: call.outputs_decoded })}
-						{/* {call.outputs_decoded &&
-							call.outputs_decoded.length > 0 &&
-							CallDetailsIo({ io: call.outputs_decoded, isOutput: true })} */}
+						{CallDetailsIo([
+							{ io: call.inputs_decoded ?? [], name: 'Input name' },
+							{ io: call.outputs_decoded ?? [], name: 'Output name' }
+						])}
 					</div>
 					<div
 						className={`h-[30rem] my-2 ${classes[call.class_hash].code ? '' : 'hidden'}`}
@@ -288,31 +249,49 @@ function CallElements(
 			);
 		}
 
+		function EventDetails({ eventDecoded }: { eventDecoded: CallEventDecoded }) {
+			return (
+				<div className="flex flex-col bg-neutral-50 rounded-b-sm border-b border-x border-neutral-200 py-1 px-2 text-sm">
+					<div className="w-fit min-w-[30rem]">
+						{CallDetailsIo([{ io: eventDecoded.data ?? [], name: 'Event argument' }])}
+					</div>
+				</div>
+			);
+		}
+
 		return (
 			<React.Fragment key={callIdentifier}>
 				<TraceLine
-					className={`border-t border-x ${
+					className={`border-t border-x cursor-pointer ${
 						expandedCalls[callIdentifier]
 							? 'rounded-t-sm rounded-b-none bg-neutral-50 border-neutral-200'
 							: 'border-transparent'
 					}`}
+					onClick={() =>
+						callExpandHandler({
+							...expandedCalls,
+							[callIdentifier]: !expandedCalls[callIdentifier]
+						})
+					}
 				>
 					{CallTypeChip(call.call_type)}
 					<div
 						style={{ marginLeft: nesting_level * CALL_NESTING_SPACE_BUMP }}
 						className="flex flex-row items-center"
 					>
-						<div className="w-5 h-5 p-1 mr-1">
+						<div className="w-5 h-5 p-1 mr-1 cursor-pointer rounded-sm hover:bg-neutral-200">
 							{call.calls.length > 0 || (call.events_decoded && call.events_decoded.length > 0) ? (
 								collapsedCalls?.[callIdentifier] == true ? (
 									<ChevronRightIcon
-										onClick={() => {
+										onClick={(event) => {
+											event.stopPropagation();
 											callCollapseHandler({ ...collapsedCalls, ...{ [callIdentifier]: false } });
 										}}
 									/>
 								) : (
 									<ChevronDownIcon
-										onClick={() => {
+										onClick={(event) => {
+											event.stopPropagation();
 											callCollapseHandler({ ...collapsedCalls, ...{ [callIdentifier]: true } });
 										}}
 									/>
@@ -321,27 +300,19 @@ function CallElements(
 								''
 							)}
 						</div>
-						<CallChip
-							className="cursor-pointer"
-							onClick={() =>
-								callExpandHandler({
-									...expandedCalls,
-									[callIdentifier]: !expandedCalls[callIdentifier]
-								})
-							}
-						>
+						<CallChip>
 							{call.contract_display_name}
 							{call.contract_data?.token_name &&
 								` (${call.contract_data?.token_name} - ${call.contract_data?.token_symbol})`}
 						</CallChip>
 						<CallChip>
 							{call.function_name ?? shortenHash(call.entry_point_selector, 13)}({' '}
-							{CallInputs(call.inputs_decoded)} )
+							{CallInputs(call.inputs_decoded, true)} )
 							{call.outputs_decoded && call.outputs_decoded.length > 0 && (
 								<>
 									<ArrowLongRightIcon className="h-3 w-3 inline mx-1" />
 									{'{ '}
-									{CallInputs(call.outputs_decoded)}
+									{CallInputs(call.outputs_decoded, true)}
 									{' }'}
 								</>
 							)}
@@ -354,15 +325,34 @@ function CallElements(
 					call.events_decoded &&
 					call.events_decoded.length > 0 &&
 					call.events_decoded.map((event_decoded, j) => (
-						<TraceLine key={j}>
-							{CallTypeChip('EVENT')}
-							<CallChip style={{ marginLeft: (nesting_level + 2.5) * CALL_NESTING_SPACE_BUMP }}>
-								{event_decoded.name}
-								{event_decoded.order ? `order=${event_decoded.order}` : ''}
-								<ArrowLongRightIcon className="h-3 w-3 inline mx-1" />{' '}
-								{CallInputs(event_decoded.data)}
-							</CallChip>
-						</TraceLine>
+						<>
+							<TraceLine
+								key={j}
+								onClick={() =>
+									callExpandHandler({
+										...expandedCalls,
+										[callIdentifier + event_decoded.name]:
+											!expandedCalls[callIdentifier + event_decoded.name]
+									})
+								}
+								className={`border-t border-x cursor-pointer ${
+									expandedCalls[callIdentifier + event_decoded.name]
+										? 'rounded-t-sm rounded-b-none bg-neutral-50 border-neutral-200'
+										: 'border-transparent'
+								}`}
+							>
+								{CallTypeChip('EVENT')}
+								<CallChip style={{ marginLeft: (nesting_level + 2.5) * CALL_NESTING_SPACE_BUMP }}>
+									{event_decoded.name}
+									{event_decoded.order ? `order=${event_decoded.order}` : ''}
+									<ArrowLongRightIcon className="h-3 w-3 inline mx-1" />{' '}
+									{CallInputs(event_decoded.data)}
+								</CallChip>
+							</TraceLine>
+							{expandedCalls[callIdentifier + event_decoded.name] && (
+								<EventDetails eventDecoded={event_decoded} />
+							)}
+						</>
 					))}
 
 				{collapsedCalls?.[callIdentifier] == true ? (
@@ -392,20 +382,18 @@ function CallElements(
 	});
 }
 
-function CallInputs(inputs?: CallIoDecoded[]) {
+function CallInputs(inputs?: CallIoDecoded[], isShorten = false) {
 	return inputs?.map((i, index) => (
 		<span key={index}>
 			{i.name && <span>{i.name}=</span>}
 			{typeof i.value === 'string' ? (
-				<span>{shortenHash(i.value)}</span>
-			) : i.type && i.type.slice(-1) === '*' ? (
-				<span>[{CallInputs(i.value)}]</span>
+				<span>{isShorten ? shortenHash(i.value) : i.value}</span>
 			) : i.value_formats && i.value_formats.DECIMAL ? (
 				<span>{i.value_formats.DECIMAL}</span>
 			) : (
 				<span>
 					{'{ '}
-					{CallInputs(i.value)}
+					{CallInputs(i.value, isShorten)}
 					{' }'}
 				</span>
 			)}
