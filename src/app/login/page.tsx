75c7224e843c1a706df95fbb3a0bb9d15f7c8ac1
Roman <barabanov1233@gmail.com>
2023-12-12 19:29:20 +0400

    Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { signIn, useSession } from 'next-auth/react';
+
+export const runtime = 'edge';
+
+export default function Page({ params }: { params: { team_id: string } }) {
+	const { status } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (status === 'authenticated') {
+			router.push('/simulations');
+		}
+	}, [status, router]);
+
+	return (
+		<div className="text-center pt-20">
+			{status === 'unauthenticated' ? (
+				<Button onClick={() => signIn('cognito')}>Sign in</Button>
+			) : status === 'loading' ? (
+				<>Loading...</>
+			) : (
+				<></>
+			)}
+		</div>
+	);
+}
