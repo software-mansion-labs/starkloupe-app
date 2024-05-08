c47b5b91582edff0b08cf48b2bab3085b0d4ce2b
Roman <barabanov1233@gmail.com>
2024-05-08 13:49:39 +0400

    Remove Simulation page

--- src/app/simulation/[id]/page.tsx
+++ src/app/simulation/[id]/page.tsx
@@ -1,7 +1,8 @@
-import { SimulationPage } from '@/components/transaction-page';
+// import { SimulationPage } from '@/components/transaction-page';
 
 export const runtime = 'edge';
 
 export default async function Page({ params }: { params: { id: string } }) {
-	return <SimulationPage simulationId={params.id} />;
+	// return <SimulationPage simulationId={params.id} />;
+	return <></>;
 }
