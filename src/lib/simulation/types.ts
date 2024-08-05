e41c79d7ca2ccfbf3686221f34744647ab347606
marijamijailovic <mijailovicmarija@hotmail.com>
2024-08-05 15:51:16 +0200

    Add cairo version in call trace

--- src/lib/simulation/types.ts
+++ src/lib/simulation/types.ts
@@ -97,6 +97,7 @@ export interface CallTrace {
 		cairoLocation?: CodeLocation; // Added on client side
 		callDebuggerData?: CallDebuggerData;
 		classHash: string; // 66 symbols format
+		cairoVersion: string;
 	};
 	nestedCallsIds: string[]; // Added on client side, list of function call id and contract call id
 }
