03fc3664e9f147002dc8b9f286cfd0df66f5a384
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-08-06 00:50:13 +0400

    Fixed metadata (#181)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,7 @@ const inter = Inter({ subsets: ['latin'] });
 const title = 'Debugger for Starknet smart contract developers | Walnut';
 const description =
 	'Delve deeper into Cairo transaction execution with our state-of-the-art debugger. Swiftly identify bugs and pinpoint areas for enhancement.';
-export const metadata: Metadata = generateMetadata(
-	title,
-	description,
-	'https://www.walnut.network/debugger'
-);
+export const metadata: Metadata = generateMetadata(title, description, 'https://app.walnut.dev/');
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
