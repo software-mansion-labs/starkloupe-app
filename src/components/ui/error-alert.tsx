10248eff46e7d8f136be4be4138212cacb072665
Melnik George <91109124+melnikga@users.noreply.github.com>
2025-08-21 14:58:56 +0400

    Error alert ui fixes (#196)

--- src/components/ui/error-alert.tsx
+++ src/components/ui/error-alert.tsx
@@ -8,9 +8,9 @@ const ErrorAlert = ({ contractCallError }: { contractCallError: ContractCall | u
 	const errorDescription = contractCallError?.errorMessage;
 	const { customSettings, updateContractColor, updateContractName } = useSettings();
 	return (
-		<Alert className="w-fit my-2 border-red-600  dark:text-white">
+		<Alert variant="compact" className="w-fit my-2 border-red-600 dark:text-white">
 			<ExclamationTriangleIcon className="h-5 w-5 !text-red-600" color="red" />
-			<AlertTitle className="">Error message: </AlertTitle>
+			<AlertTitle className="!font-light">Error message: </AlertTitle>
 			<AlertDescription>
 				<span className="text-red-600">{errorDescription}</span> in{' '}
 				{contractCallError && (
