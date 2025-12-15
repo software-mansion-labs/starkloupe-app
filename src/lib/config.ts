f498a81c504b62a02b6df43526f82dc982033767
Melnik George <melnik.goshan2013@gmail.com>
2025-12-15 22:35:18 +0400

    Migrate from logto to betterauth

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,9 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'https://api.walnut.dev';
 export const WALNUT_VERIFY_DOCS_URL =
 	'https://docs.walnut.dev/overview/verify-starknet-contracts-in-walnut';
+export const VERIFY_URL =
+	process.env.NODE_ENV === 'production'
+		? 'https://verify.walnut.dev'
+		: 'http://verify.walnut.local';
+export const REPO_URL =
+	process.env.NODE_ENV === 'production' ? 'https://repo.walnut.dev' : 'http://repo.walnut.local';
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,36 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import { isLogged } from '@/app/api/auth/auth-service';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 /**
  * Router middleware. Configure here URLs to which user has/not have access (logged/not logged).
  */
 export async function middleware(request: NextRequest) {
-    const isUserLogged = await isLogged();
-    const openPageOrGoToLoginIfNotLogged = async (url: URL) => {
-        return isUserLogged ? NextResponse.next() : NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/login`);
-    };
-    const url = new URL(request.url);
-    switch (url.pathname) {
-        case '/':
-        case '/settings':
-            return openPageOrGoToLoginIfNotLogged(url);
-        default: {
-            if (url.pathname.startsWith('/monitoring')) {
-                return openPageOrGoToLoginIfNotLogged(url);
-            }
-            return NextResponse.next();
-        }
-    }
+	const sessionCookie = request.cookies.get(
+		`${process.env.NODE_ENV === 'production' ? '__Secure-' : ''}better-auth.session_token`
+	);
+	const isLogged = !!sessionCookie;
+
+	const openPageOrGoToLoginIfNotLogged = async (url: URL) => {
+		return isLogged
+			? NextResponse.next()
+			: NextResponse.redirect(
+					`${
+						process.env.NODE_ENV === 'production'
+							? 'https://app.walnut.dev'
+							: 'http://localhost:5173'
+					}/login`
+			  );
+	};
+	const url = new URL(request.url);
+	switch (url.pathname) {
+		case '/':
+		case '/settings':
+			return openPageOrGoToLoginIfNotLogged(url);
+		default: {
+			if (url.pathname.startsWith('/monitoring')) {
+				return openPageOrGoToLoginIfNotLogged(url);
+			}
+			return NextResponse.next();
+		}
+	}
 }
