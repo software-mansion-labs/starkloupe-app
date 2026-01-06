589e429eb1922ef1b0ac21b7936491d1d167d4e8
Melnik George <melnik.goshan2013@gmail.com>
2026-01-06 19:27:07 +0400

    Removed logto

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,5 @@ import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL || '');
 export const db = drizzle(sql, { schema });
