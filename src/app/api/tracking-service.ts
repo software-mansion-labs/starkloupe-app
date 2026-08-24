2f953ab04221e582ccbd45d17885312d381657a3
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2026-08-24 14:25:20 +0200

    Enable tracking

--- src/app/api/tracking-service.ts
+++ src/app/api/tracking-service.ts
@@ -2,17 +2,17 @@ export const isTrackingActive = () => {
 	if (typeof window === 'undefined') {
 		return true;
 	}
-	// Check if the URL has skipTracking=true in the query params
+	// Check if the URL has skip_tracking=true in the query params
 	const urlParams = new URLSearchParams(window.location.search);
-	const shouldSkipSentryBasedOnQueryParam = urlParams.get('skip_tracking') === 'true';
+	const shouldSkipBasedOnQueryParam = urlParams.get('skip_tracking') === 'true';
 
-	// Also to disable Sentry on client - set 'skip-sentry-pls=true' cookie
+	// Also to disable tracking on client - set 'skip_tracking_pls=true' cookie
 	const cookies = document.cookie.split(';');
-	const shouldSkipSentryBasedOnCookie = cookies.some((cookie) =>
+	const shouldSkipBasedOnCookie = cookies.some((cookie) =>
 		cookie.trim().startsWith('skip_tracking_pls=true')
 	);
 
-	// Sentry starts only on client if NEXT_PUBLIC_USE_TRACKING is set to 'true'
-	const isProdEnv = process.env.NEXT_PUBLIC_USE_TRACKING === 'true';
-	return !shouldSkipSentryBasedOnCookie && !shouldSkipSentryBasedOnQueryParam && isProdEnv;
+	// Tracking is on only if NEXT_PUBLIC_USE_TRACKING was 'true' at build time
+	const isTrackingBuild = process.env.NEXT_PUBLIC_USE_TRACKING === 'true';
+	return !shouldSkipBasedOnCookie && !shouldSkipBasedOnQueryParam && isTrackingBuild;
 };
