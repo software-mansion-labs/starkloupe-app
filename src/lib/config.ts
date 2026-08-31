4b0fbe16221b9a5c578865c1c118a1229445fa7b
maciektr <mtratnowiecki@gmail.com>
2026-08-31 22:58:37 +0200

    Drop unused repo service url (#7)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,2 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'https://api.starkloupe.co';
 export const WALNUT_VERIFY_DOCS_URL = 'https://docs.walnut.dev/verification/verify-starknet';
-export const REPO_URL =
-	process.env.NODE_ENV === 'production' ? 'https://repo.walnut.dev' : 'http://repo.walnut.local';
